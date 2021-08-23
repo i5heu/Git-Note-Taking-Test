870e97d22bd69beee5c179841d693e5e5d30904b
i5heu <i5heu@i5h.eu>
2021-08-23 20:28:36 +0200

    Test

---
+++ .scripts/add_hash_evrywere.js
@@ -0,0 +1,9 @@
+async function main() {
+    await tree.iterateOverTree(async (chunk) => {
+        if(chunk.subItems.includes(mainChunk)){
+            console.log(chunk);
+        }
+    });
+}
+
+main();
