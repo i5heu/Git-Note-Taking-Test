6b0c810c3f52f82205b8921b70733e291d244459
i5heu <i5heu@i5h.eu>
2021-08-25 11:33:44 +0200

    GitBot

--- .scripts/add_hash_evrywere.js
+++ .scripts/add_hash_evrywere.js
@@ -1,7 +1,7 @@
 async function main() {
     await tree.iterateOverTree(async (chunk) => {
         if(chunk.subItems.includes(mainChunk)){
-            console.log(chunk);
+            // console.log(chunk);
         }
     });
 }
