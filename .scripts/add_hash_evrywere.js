37e3c2e19ade98878ed5895117df903aa11f0b01
i5heu <i5heu@i5h.eu>
2021-08-28 17:30:45 +0200

    add tables and work on scripting

--- .scripts/add_hash_evrywere.js
+++ .scripts/add_hash_evrywere.js
@@ -1,9 +1,24 @@
-async function main() {
+async function findTable() {
+    let foo;
+
     await tree.iterateOverTree(async (chunk) => {
-        if(chunk.subItems.includes(mainChunk)){
-            // console.log(chunk);
-        }
+        chunk.subItems.forEach(element => {
+            if (element.name && element.name.includes(".calcSum.")) {
+                foo = element;
+            }
+        });
     });
+
+    return foo;
+}
+
+async function main() {
+    const element = await findTable();
+    var data = await getContentOfFile(element.path, "utf8");
+
+    console.log("add_hash_evrywere.js", await findMarkdownTable(data));
+
+    console.log("md",md);
 }
 
 main();
